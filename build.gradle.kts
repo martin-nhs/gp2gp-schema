714937e1d89e79516683f6490cf2e36c1c6100b3
martin-nhs <127403254+martin-nhs@users.noreply.github.com>
2024-08-27 10:23:14 +0100

    Initial Commit

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    id("java-library")
+    id("maven-publish")
+}
+
+group = "uk.nhs.nia"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "gp2gp-schema"
+
