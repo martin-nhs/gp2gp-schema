137bc2a3f31acb2842a865dad7876d6ed8e723d7
martin-nhs <127403254+martin-nhs@users.noreply.github.com>
2024-08-27 10:25:46 +0100

    Add Jakarta XML Binding specification and Eclipse implementation

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,9 @@ repositories {
 }
 
 dependencies {
+    implementation("jakarta.xml.bind:jakarta.xml.bind-api:4.0.2")
+    implementation("com.sun.xml.bind:jaxb-impl:4.0.5")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
