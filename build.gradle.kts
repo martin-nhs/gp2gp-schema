d4775630cca9d9437395d65e74dd9ff3ccd2411a
martin-nhs <127403254+martin-nhs@users.noreply.github.com>
2024-08-27 10:45:21 +0100

    Migrate initial unit tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,11 +12,12 @@ repositories {
 
 dependencies {
     implementation("jakarta.xml.bind:jakarta.xml.bind-api:4.0.2")
-    implementation("com.sun.xml.bind:jaxb-impl:4.0.5")
     implementation("org.apache.commons:commons-lang3:3.16.0")
+    implementation("com.sun.xml.bind:jaxb-impl:4.0.5")
 
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.assertj:assertj-core:3.26.3")
 }
 
 tasks.test {
---
+++ src/test/java/JaxbTest.java
@@ -0,0 +1,104 @@
+
+import java.io.File;
+
+import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.JAXBElement;
+import jakarta.xml.bind.JAXBException;
+import jakarta.xml.bind.Unmarshaller;
+
+import org.junit.jupiter.api.Test;
+
+import uk.nhs.nia.gp2gp.schema.org.hl7.v3.CV;
+import uk.nhs.nia.gp2gp.schema.org.hl7.v3.RCMRIN030000UKMessage;
+import uk.nhs.nia.gp2gp.schema.org.hl7.v3.RCMRMT030101UKEhrFolder;
+import utility.FileFactory;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class JaxbTest {
+    private static final String RCMR_IN030000UK06_TEST_FILE_NAME = "RCMR_IN030000UK06.xml";
+    private static final String RCMR_IN030000UK07_TEST_FILE_NAME = "RCMR_IN030000UK07.xml";
+
+    @Test
+    void When_RCMRIN030000UK06MessageIsUnmarshalled_Expect_FieldsToBeParsable() throws JAXBException {
+        // given
+        final File file = FileFactory.getFileFor(RCMR_IN030000UK06_TEST_FILE_NAME);
+        JAXBElement<RCMRIN030000UKMessage> unmarshalledMessage = unmarshallFromFile(file, RCMRIN030000UKMessage.class);
+
+        // when
+        var ehrFolder = unmarshalledMessage
+            .getValue()
+            .getControlActEvent()
+            .getSubject()
+            .getEhrExtract()
+            .getComponent()
+            .getFirst()
+            .getEhrFolder();
+
+        var person = ehrFolder.getResponsibleParty().getAgentDirectory().getPart().get(2).getAgent().getAgentPerson();
+        var place = ehrFolder.getComponent().getFirst().getEhrComposition().getLocation().getLocatedEntity().getLocatedPlace();
+
+        // then
+        assertThat(person.getName().getFamily()).isEqualTo("Whitcombe");
+        assertThat(person.getName().getGiven()).isEqualTo("Peter");
+        assertThat(person.getName().getPrefix()).isEqualTo("Dr");
+        assertThat(person.getName().getValidTime().getCenter().getValue()).isEqualTo("20100114");
+        assertThat(place.getName()).isEqualTo("EMIS Test Practice Location");
+    }
+
+    @Test
+    void When_RCMRIN030000UK07MessageUnmarshalled_Expect_ConfidentialityCodePresent() throws JAXBException {
+        // given
+        final File file = FileFactory.getFileFor(RCMR_IN030000UK07_TEST_FILE_NAME);
+        final JAXBElement<RCMRIN030000UKMessage> unmarshalledMessage = unmarshallFromFile(file, RCMRIN030000UKMessage.class);
+        final String code = "NOPAT";
+        final String codeSystem = "2.16.840.1.113883.4.642.3.47";
+        final String displayName = "no disclosure to patient, family or caregivers without attending provider's authorization";
+
+        // when
+        final RCMRMT030101UKEhrFolder ehrFolder = unmarshalledMessage
+            .getValue()
+            .getControlActEvent()
+            .getSubject()
+            .getEhrExtract()
+            .getComponent()
+            .getFirst()
+            .getEhrFolder();
+
+        final CV observationStatementConfidentialityCode = ehrFolder
+            .getComponent()
+            .get(0)
+            .getEhrComposition()
+            .getComponent()
+            .getFirst()
+            .getObservationStatement()
+            .getConfidentialityCode()
+            .orElseThrow();
+
+        final CV medicationStatementConfidentialityCode = ehrFolder
+            .getComponent()
+            .get(2)
+            .getEhrComposition()
+            .getComponent()
+            .get(1)
+            .getMedicationStatement()
+            .getConfidentialityCode()
+            .orElseThrow();
+
+        // then
+        assertThat(medicationStatementConfidentialityCode.getCode()).isEqualTo(code);
+        assertThat(medicationStatementConfidentialityCode.getCodeSystem()).isEqualTo(codeSystem);
+        assertThat(medicationStatementConfidentialityCode.getDisplayName()).isEqualTo(displayName);
+        assertThat(observationStatementConfidentialityCode.getCode()).isEqualTo(code);
+        assertThat(observationStatementConfidentialityCode.getCodeSystem()).isEqualTo(codeSystem);
+        assertThat(observationStatementConfidentialityCode.getDisplayName()).isEqualTo(displayName);
+    }
+
+    // Helper Methods
+    private <T> JAXBElement<T> unmarshallFromFile(File source, Class<T> destination) throws JAXBException {
+        final JAXBContext context = JAXBContext.newInstance(destination);
+        final Unmarshaller unmarshaller = context.createUnmarshaller();
+
+        return (JAXBElement<T>) unmarshaller.unmarshal(source);
+    }
+}
